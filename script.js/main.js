faebab5952102fc8c8bf7879d7d65745d001bb04
ChisomNgwu <164569337+ChisomNgwu@users.noreply.github.com>
2024-06-16 01:22:00 +0100

    valutico_webpage_

---
+++ script.js/main.js
@@ -0,0 +1,26 @@
+const  mobile_menu = document.querySelector('.mobile-nav');
+const  popBar = document.getElementById('popBar');
+const  collapseBar = document.getElementById('collapseBar');
+
+popBar.addEventListener('click', function(){
+        if(mobile_menu.style.display === 'none'){
+            mobile_menu.style.display = 'block';
+            popBar.style.display = 'none';
+            collapseBar.style.display = 'block';
+        } else {
+            mobile_menu.style.display = 'none';
+            popBar.style.display = 'block';
+            collapseBar.style.display = 'none';
+        }
+});
+
+collapseBar.addEventListener('click',
+    function(){
+        if(mobile_menu.style.display === 'block'){
+            mobile_menu.style.display = 'none';
+            popBar.style.display = 'block';
+            collapseBar.style.display = 'none';
+        }
+});
+
+ 
